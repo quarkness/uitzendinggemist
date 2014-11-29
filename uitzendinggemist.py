4ab1b2b391d2ca08e51e1c0f383a1f2a8aa07a42
Therry van Neerven <therryvanneerven@gmail.com>
2014-11-29 17:47:34 +0100

    Fixed downloading from NPO.nl, PEP8 fixes, increased chunk size (lower disk io) and progress bar, tranlated dutch words

--- uitzendinggemist.py
+++ uitzendinggemist.py
@@ -3,9 +3,11 @@ import re
 from bs4 import BeautifulSoup
 from clint.textui import progress
 import requests
-import sys
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36'
+progress.BAR_TEMPLATE = BAR_TEMPLATE = '%s[%s%s] %i/%i MB - %s\r'  # Completed parts shown as MB
+USER_AGENT = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) '
+              + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36')
+
 
 class UitzendingGemist(object):
     def __init__(self):
@@ -17,93 +19,103 @@ class UitzendingGemist(object):
     def load(self):
         self.data = self.rs.get(self.data_url).json()
 
+
 class Serie(UitzendingGemist):
 
     def __init__(self, nebo_id=None):
         super(Serie, self).__init__()
-        self.afleveringen = []
+        self.episodes = []
         if nebo_id:
             self.nebo_id = nebo_id
             self.data_url = 'http://apps-api.uitzendinggemist.nl/series/{}.json'.format(self.nebo_id)
             self.load()
-            self.afleveringen = [Aflevering(a['nebo_id'], a['name'], self) for a in self.data['episodes']]
-            self.naam = self.data['name']
-            self.beschrijving = self.data['description']
+            self.episodes = [Episode(episode['nebo_id'], episode['name'], self) for episode in self.data['episodes']]
+            self.name = self.data['name']
+            self.description = self.data['description']
 
     def __unicode__(self):
-        return '{} [{}]'.format(self.naam, self.nebo_id)
+        return '{} [{}]'.format(self.name, self.nebo_id)
 
     @staticmethod
     def by_rss(url, limit=10):
-        serie = Serie()
+        serie = Episode()
         rss = BeautifulSoup(UitzendingGemist().rs.get(url).content, "xml")
         for item in rss.find_all('item')[:limit]:
-            print item.title.text
-            print item.link.text
+            print(item.title.text)
+            print(item.link.text)
             nebo_id = item.guid.text.replace('http://gemi.st/', '')
-            naam = ' - '.join(item.title.text.split(' - ')[1:])
-            aflevering = Aflevering(nebo_id, naam=naam, serie_naam=rss.channel.title.text)
-            serie.afleveringen.append(aflevering)
+            name = ' - '.join(item.title.text.split(' - ')[1:])
+            episode = Episode(nebo_id, name=name, serie_name=rss.channel.title.text)
+            serie.episodes.append(episode)
         return serie
 
-class Aflevering(UitzendingGemist):
-    def __init__(self, nebo_id, naam='', serie=None, serie_naam=None):
-        super(Aflevering, self).__init__()
+class Episode(UitzendingGemist):
+    def __init__(self, nebo_id, name='', serie=None, serie_name=None):
+        super(Episode, self).__init__()
         self.playerid = nebo_id
-        self.naam = naam
+        self.name = name
         self.serie = serie
-        self.serie_naam = serie_naam
+        self.serie_name = serie_name
 
     def __unicode__(self):
-        return '{} - {} [{}]'.format(self.serienaam, self.naam, self.playerid)
+        return '{} - {} [{}]'.format(self.serie_name, self.name, self.playerid)
 
     @staticmethod
     def by_url(url):
-        h = requests.get(url)
-        soup = BeautifulSoup(h.content)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content)
         nebo_id = url.split('/')[-1]
-        meta = soup.find('div', {'class': 'visible-no-grid-collapse'})
 
-        serie_naam = meta.h1.a.text
-        naam = meta.h2.text
-        return Aflevering(nebo_id, naam, serie_naam=serie_naam)
+        serie_name = soup.find('div', {'class': 'row-fluid title-and-broadcasters'}).h1.a.text
+
+        try:
+            name = soup.find('div', {'class': 'span-sidebar'}).h2.text
+        except AttributeError:  # Episode name is not always available
+            name = None
+
+        return Episode(nebo_id, name, serie_name=serie_name)
 
     @property
-    def serienaam(self):
+    def seriename(self):
         if self.serie:
-            return self.serie.naam
+            return self.serie.name
         else:
-            return self.serie_naam
+            return self.serie_name
 
     @property
-    def bestandsnaam(self):
-        return u'{} - {} - {}.mp4'.format(self.serienaam, self.naam, self.playerid).replace('/', '_')
+    def filename(self):
+        raw_name = u'{} - {} - {}.mp4'.format(self.seriename, self.name, self.playerid) if self.name \
+            else u'{} - {}.mp4'.format(self.seriename, self.playerid)
+        return raw_name.replace('/', '_')
 
     def get_token(self):
         data = self.rs.get('http://ida.omroep.nl/npoplayer/i.js').content
         return re.compile('.token\s*=\s*"(.*?)"', re.DOTALL + re.IGNORECASE).search(str(data)).group(1)
 
     def get_streams(self, token):
-        url = 'http://ida.omroep.nl/odi/?prid={}&puboptions=h264_bb,h264_std,h264_sb&adaptive=no&part=1&token={}'.format(self.playerid, token)
+        url = ('http://ida.omroep.nl/odi/' +
+               '?prid={}&puboptions=h264_bb,h264_std,h264_sb&adaptive=no&part=1&token={}').format(self.playerid, token)
         return self.rs.get(url).json()['streams']
 
     def get_url(self):
         token = self.get_token()
         streams = self.get_streams(token)
         json_url = streams[0].replace('jsonp', 'json')
-        print 'json_url: {}'.format(json_url)
+        print('json_url: {}'.format(json_url))
         url = self.rs.get(json_url).json()['url']
-        print 'url: {}'.format(url)
+        print('url: {}'.format(url))
         return url
 
     def download(self):
         url = self.get_url()
         r = self.rs.get(url, stream=True)
-        print r.status_code
-        with open(self.bestandsnaam, 'wb') as f:
+        chunk_size = 1024**2  # 1MB
+        print('HTTP status code: {}'.format(r.status_code))
+        with open(self.filename, 'wb') as f:
             total_length = int(r.headers.get('content-length'))
-            for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1):
-                if chunk: # filter out keep-alive new chunks
+            for chunk in progress.bar(
+                    r.iter_content(chunk_size=chunk_size), expected_size=(total_length/chunk_size) + 1):
+                if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
                     f.flush()
-        print ''
+        print('Finished downloading {}'.format(self.filename))
